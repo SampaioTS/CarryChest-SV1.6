582486f2ff0d99dc7d02c3684d01d29a5eaf0d58
SampaioTS <jsl_sampaio@hotmail.com>
2024-03-24 13:56:55 +0000

    Add files via upload
    "Fake fork" from the original repository: https://github.com/spacechase0/StardewValleyMods

---
+++ CarryChest/Patches/ItemPatcher.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics.CodeAnalysis;
+using CarryChest.Framework;
+using HarmonyLib;
+using Spacechase.Shared.Patching;
+using SpaceShared;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Objects;
+
+namespace CarryChest.Patches
+{
+    /// <summary>Applies Harmony patches to <see cref="Item"/>.</summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = DiagnosticMessages.NamedForHarmony)]
+    internal class ItemPatcher : BasePatcher
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <inheritdoc />
+        public override void Apply(Harmony harmony, IMonitor monitor)
+        {
+            harmony.Patch(
+                original: this.RequireMethod<Item>(nameof(Item.canStackWith)),
+                postfix: this.GetHarmonyMethod(nameof(After_CanStackWith))
+            );
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method to call after <see cref="Item.canStackWith"/>.</summary>
+        private static void After_CanStackWith(Item __instance, ISalable other, ref bool __result)
+        {
+            // prevent stacking chests that contain items
+            if (__result)
+                __result = !ItemPatcher.ShouldPreventStacking(__instance) && !ItemPatcher.ShouldPreventStacking(other);
+        }
+
+        /// <summary>Get whether to prevent stacking the given item.</summary>
+        /// <param name="item">The item to check.</param>
+        private static bool ShouldPreventStacking(ISalable item)
+        {
+            return
+                ChestHelper.IsSupported(item)
+                && item is Chest chest
+                && chest.Items.Count != 0;
+        }
+    }
+}
---
+++ CarryChest/Patches/ObjectPatcher.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics.CodeAnalysis;
+using CarryChest.Framework;
+using HarmonyLib;
+using Spacechase.Shared.Patching;
+using SpaceShared;
+using StardewModdingAPI;
+using StardewValley.Objects;
+using SObject = StardewValley.Object;
+
+namespace CarryChest.Patches
+{
+    /// <summary>Applies Harmony patches to <see cref="SObject"/>.</summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = DiagnosticMessages.NamedForHarmony)]
+    internal class ObjectPatcher : BasePatcher
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <inheritdoc />
+        public override void Apply(Harmony harmony, IMonitor monitor)
+        {
+            harmony.Patch(
+                original: this.RequireMethod<SObject>(nameof(SObject.getDescription)),
+                postfix: this.GetHarmonyMethod(nameof(After_GetDescription))
+            );
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method to call before <see cref="SObject.getDescription"/>.</summary>
+        private static void After_GetDescription(SObject __instance, ref string __result)
+        {
+            if (ChestHelper.IsSupported(__instance))
+            {
+                var chest = __instance as Chest;
+                __result += "\n" + $"Contains {chest?.Items?.Count ?? 0} items.";
+            }
+        }
+    }
+}
